b3631b7781143d9ea528784912d9bb43ff920349
javorkabotond <botondjvorka@gmail.com>
2020-10-02 17:16:58 +0200

    Deleted files and create basic task

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const todoList = document.querySelector('#todo_list');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const taskDiv = document.createElement('div');
+
+const newListElem = document.createElement('li');
+const checkbox = document.createElement('input');
+checkbox.type = 'checkbox';
+newListElem.appendChild(checkbox);
+newListElem.innerHTML = 'Task 1';
+
+const editButton = document.createElement("button");
+editButton.innerHTML = 'Edit';
+editButton.classList.add("edit_button");
+newListElem.appendChild(editButton);
+const deleteButton = document.createElement("button");
+deleteButton.innerHTML = 'Delete';
+deleteButton.classList.add("delete_button");
+newListElem.appendChild(deleteButton);
+
+taskDiv.appendChild(newListElem);
+todoList.appendChild(taskDiv);
