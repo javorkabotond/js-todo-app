5281c2671819d297309b80cca215deabd64ba6c8
javorkabotond <botondjvorka@gmail.com>
2020-10-02 22:23:13 +0200

    Added basic html and style

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const todoList = document.querySelector('#todo_list');
+// const todoList = document.querySelector('#todo_list');
 
 
-const taskDiv = document.createElement('div');
+// const taskDiv = document.createElement('div');
 
-const newListElem = document.createElement('li');
-const checkbox = document.createElement('input');
-checkbox.type = 'checkbox';
-newListElem.appendChild(checkbox);
-newListElem.innerHTML = 'Task 1';
+// const newListElem = document.createElement('li');
+// const checkbox = document.createElement('input');
+// checkbox.type = 'checkbox';
+// newListElem.appendChild(checkbox);
+// newListElem.innerHTML = 'Task 1';
 
-const editButton = document.createElement("button");
-editButton.innerHTML = 'Edit';
-editButton.classList.add("edit_button");
-newListElem.appendChild(editButton);
-const deleteButton = document.createElement("button");
-deleteButton.innerHTML = 'Delete';
-deleteButton.classList.add("delete_button");
-newListElem.appendChild(deleteButton);
+// const editButton = document.createElement("button");
+// editButton.innerHTML = 'Edit';
+// editButton.classList.add("edit_button");
+// newListElem.appendChild(editButton);
+// const deleteButton = document.createElement("button");
+// deleteButton.innerHTML = 'Delete';
+// deleteButton.classList.add("delete_button");
+// newListElem.appendChild(deleteButton);
 
-taskDiv.appendChild(newListElem);
-todoList.appendChild(taskDiv);
+// taskDiv.appendChild(newListElem);
+// todoList.appendChild(taskDiv);
