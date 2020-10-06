2cf76e6fee8332f2f457d8dd0c48643d85f520de
javorkabotond <botondjvorka@gmail.com>
2020-10-06 15:33:41 +0200

    Created add new todo function

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-// const todoList = document.querySelector('#todo_list');
+const todoList = document.querySelector('#todo_list');
+const addBtn = document.querySelector('#add_btn');
 
 
-// const taskDiv = document.createElement('div');
+let todoItems = [];
 
-// const newListElem = document.createElement('li');
-// const checkbox = document.createElement('input');
-// checkbox.type = 'checkbox';
-// newListElem.appendChild(checkbox);
-// newListElem.innerHTML = 'Task 1';
+function addNewTodo(text){
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now()
+  };
 
-// const editButton = document.createElement("button");
-// editButton.innerHTML = 'Edit';
-// editButton.classList.add("edit_button");
-// newListElem.appendChild(editButton);
-// const deleteButton = document.createElement("button");
-// deleteButton.innerHTML = 'Delete';
-// deleteButton.classList.add("delete_button");
-// newListElem.appendChild(deleteButton);
+  todoItems.push(todo);
+  console.log(todo)
+}
 
-// taskDiv.appendChild(newListElem);
-// todoList.appendChild(taskDiv);
+addBtn.addEventListener('click', event => {
+  event.preventDefault();
+  
+  const input = document.querySelector('#input_field');
+  const text = input.value.trim();
+  if(text !== '') {
+    addNewTodo(text);
+    input.value = '';
+  }
+})
