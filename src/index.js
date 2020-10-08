da574b776d521e42dba14bd78cd809056df5bd62
javorkabotond <botondjvorka@gmail.com>
2020-10-08 15:35:06 +0200

    Changed all id to class, deleted innerHTML and deleted <i> html tag

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-const { createElement } = require("react");
-
-const addBtn = document.querySelector('#add_btn');
+const addBtn = document.querySelector('.add_btn');
 
 let todoItems = [];
 
-function addNewTodo(text){
+function addNewTodo(text) {
   const todo = {
     text,
     checked: false,
@@ -18,26 +16,42 @@ function addNewTodo(text){
 
 addBtn.addEventListener('click', event => {
   event.preventDefault();
-  
-  const input = document.querySelector('#input_field');
+
+  const input = document.querySelector('.input_field');
   const text = input.value.trim();
-  if(text !== '') {
+  if (text !== '') {
     addNewTodo(text);
     input.value = '';
   }
 })
 
 function renderTodo(todo) {
-  const todoList = document.querySelector('#todo_list');
+  const todoList = document.querySelector('.todo_list');
 
   const article = document.createElement('article');
   article.setAttribute('class', 'list_elem');
   article.setAttribute('data-key', todo.id);
-  article.innerHTML = `
-    <button class="complete_btn"><i class="fas fa-check"></i></button>
-    <li class="task">${todo.text}</li>
-    <button class="edit_btn"><i class="fas fa-pen"></i></button>
-    <button class="delete_btn"><i class="fas fa-trash"></i></button>
-  `;
+
+  const completeBtn = document.createElement('button');
+  completeBtn.setAttribute('class', 'complete_btn');
+  completeBtn.innerText = 'C';
+
+  const listElem = document.createElement('li');
+  listElem.setAttribute('class', 'task');
+  listElem.innerText = todo.text;
+
+  const editBtn = document.createElement('button');
+  editBtn.setAttribute('class', 'edit_btn');
+  editBtn.innerText = 'E';
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'delete_btn');
+  deleteBtn.innerText = 'D';
+
+  article.appendChild(completeBtn);
+  article.appendChild(listElem);
+  article.appendChild(editBtn);
+  article.appendChild(deleteBtn);
+
   todoList.appendChild(article)
 }
